c6d859c5a1a91a6d0ff2ba5f8972d7d9cd270ce4
Taher-Abu-Tayyem <tayyem2000@hotmail.com>
2026-04-14 13:29:54 +0300

    theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,27 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState("Task One")
+  const [Task, setTask] = useState("Task One")
+  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState("dark")
+
+
 
 
   return (
-         <div> 
-          <button onClick={() => setCount("Task Two")}>{count}</button>
+         <div className={theme}> 
+          <button onClick={() => setTask("Task Two")}>{Task}</button>
           <br />
-          <button onClick={() => setCount("Task One")}>rest</button>
+          <button onClick={() => setTask("Task One")}>rest Task</button>
+                    <br />
+          <button onClick={() => setCount(count + 1)}>{count}+</button>
+                    <br />
+            <button onClick={() => setCount(count - 1)}>{count}-</button>
+                    <br />  
+          <button onClick={() => setCount(0)}>rest counter</button>
+                    <br />
+          <button onClick={() => setTheme("light")}>light</button>
+          <button onClick={() => setTheme("dark")}>dark</button>
           
         </div>
     
