6cd3412f5bbe09bf4c1edfefb0b51ae159798587
Taher-Abu-Tayyem <tayyem2000@hotmail.com>
2026-04-17 23:09:15 +0300

    Use Reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer, useState } from "react";
 
-import './App.css'
-import { MdNightlight } from 'react-icons/md';
+import "./App.css";
+import { MdNightlight } from "react-icons/md";
 import { FaRegSun } from "react-icons/fa6";
 
+const initialScore = 
+  {
+    Field: "React",
+    level: "mid",
+    count: 0,
+    theme: "light",
+    color: "red",
+  }
+;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "Changecountplus":
+      return { ...state, count: action.newcount  };
+    case "Changecount":
+      return { ...state, count: action.newcount };
+    case "Reset":
+      return { ...state, count: 0 };
+    case "ChangeField":
+      return { ...state, Field: action.newField };
+    case "CHANGE_LEVEL":
+      return { ...state, level: action.newlevel };
+    case "CHANGE_THEME":
+      return { ...state, theme: action.newTheme ,color: action.newcolor};
+    
+    default:
+      return state;
+  }
+};
 function App() {
+  /*
   const [Task, setTask] = useState("Task One")
   const [count, setCount] = useState(0)
 
-
+ 
  // 👇 نخزن الثيم من localStorage أول ما يفتح الموقع
   const [theme, setTheme] = useState(() => {
     return localStorage.getItem("theme") || "dark";
@@ -18,33 +48,62 @@ function App() {
   useEffect(() => {
     localStorage.setItem("theme", theme);
   }, [theme]);
+ */
+
+  const [Data, dispatch] = useReducer(reducer, initialScore);
 
   return (
-         <div className={theme}> 
-          <button onClick={() => setTask("Task Two")}>{Task}</button>
-          <br />
-          <button onClick={() => setTask("Task One")}>rest Task</button>
-                    <br />
-          <button onClick={() => setCount(count + 1)}>{count}+</button>
-                    <br />
-            <button onClick={() => setCount(count - 1)}>{count}-</button>
-                    <br />  
-          <button onClick={() => setCount(0)}>rest counter</button>
-                    <br />
-          <button onClick={() => setTheme("light")}>light</button>
-          <button onClick={() => setTheme("dark")}>dark</button>
-          <br /> 
+    <div className={Data.theme} >
+      <h1 style={{ color: Data.color }}>My level is{Data.Field}</h1>
+      <br />
+      <button
+        onClick={() => {
+          dispatch({ type: "ChangeField", newField: "Anagular" });
+        }}
+      >
+        Change Field
+      </button>
+      <br />
+      <button
+        onClick={() =>
+          dispatch({ type: "Changecountplus", newcount: Data.count + 1 })
+        }
+      >
+        {" "}
+        Counter {Data.count}+
+      </button>
+      <br />
+      <button
+        onClick={() => dispatch({ type: "CHANGE_LEVEL", newlevel: "High" })}
+      >
+        My level is {Data.level}
+      </button>
 
-          <div>
-            <button onClick={   () => {
-              setTheme(theme === "light" ? "dark" : "light")
-            }
-            } style={{marginTop:"30px"}}>{theme === "light" ? <MdNightlight /> : <FaRegSun />}</button>
-          </div>
-        </div>
-        
+      <br />
+      <button
+        onClick={() =>
+          dispatch({ type: "Changecount", newcount: Data.count - 1 })
+        }
+      >
+      
+        Counter {Data.count}-
+      </button>
+      <br />
+      <button onClick={() => dispatch({ type: "Reset", newcount: 0 })}>
+        rest counter
+      </button>
+      <br />
+      
+      <button
+        onClick={() => dispatch({ type: "CHANGE_THEME", newTheme: "dark" ,newcolor:"White" })}
+      >
+        dark
+      </button>
+      <br />
+
     
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
